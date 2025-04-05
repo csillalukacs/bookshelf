3b22a3689716ac7b880e6bcb2a398f9ad3a25eb5
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-04-05 21:19:27 +0200

    button style

--- src/app/book/[id]/DeleteBook.tsx
+++ src/app/book/[id]/DeleteBook.tsx
@@ -4,7 +4,13 @@ import { deleteBook } from "@/app/actions/book-actions";
 import { Book } from "@/app/lib/definitions";
 import Button from "@/components/Button";
 
-export function DeleteBookButton({book} : {book: Book}) 
+export function DeleteBookButton({ book }: { book: Book }) 
 {
-    return <Button label={"Delete this book"} onClick={() => deleteBook(book.id)} disabled={false} />;
+    return <Button
+        label={"Delete this book"}
+        onClick={() => deleteBook(book.id)}
+        disabled={false}
+        red={true}
+        type="text"
+    />;
 }
