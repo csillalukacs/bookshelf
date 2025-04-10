3291e59a21c0a184eebb907dcbb1fb4e9d9fe86b
Csilla Lukacs <erzsebet.lk@gmail.com>
2025-04-10 23:03:04 +0200

    renamed pool to globalPool

--- src/app/postgres.ts
+++ src/app/postgres.ts
@@ -1,12 +1,12 @@
 import { Pool } from "pg";
 
 declare global {
-  var pool: Pool | undefined;
+  var globalPool: Pool | undefined;
 }
 
 // Ensure the pool is created only once
 export const pool =
-  global.pool ||
+  global.globalPool ||
   new Pool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
@@ -17,7 +17,7 @@ export const pool =
     connectionTimeoutMillis: 2000,
   })
 
-if (process.env.NODE_ENV !== "production") global.pool = pool;
+if (process.env.NODE_ENV !== "production") global.globalPool = pool;
 
 console.log("Pool total count, idle count, waiting count: ",
    pool.totalCount, pool.idleCount, pool.waitingCount)
